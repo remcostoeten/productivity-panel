1f50a2bbf9d5a7730d706374859f88e2e024c4a7
Remco Stoeten <remcostoeten@hotmail.com>
2024-08-06 01:16:35 +0200

    feat: add clerk dakr mode auth

--- src/core/lib/providers.tsx
+++ src/core/lib/providers.tsx
@@ -1,9 +1,19 @@
 import { TooltipProvider } from "@/components/ui";
 import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 export default function Providers({ children }: PageProps) {
     return (
-        <ClerkProvider>
+        <ClerkProvider
+            appearance={{
+                baseTheme: [dark],
+                elements: {
+                    userButtonPopoverMain: 'bg-gray-subtle',
+                    navbar: 'bg-gradient-to-r from-gray-subtle to-gray-subtle',
+                    pageScrollBox: 'bg-gray-subtle',
+                },
+            }}
+        >
             <TooltipProvider>
                 {children}
             </TooltipProvider>
