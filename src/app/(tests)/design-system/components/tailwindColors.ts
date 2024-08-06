bd84cc753db6230346eb791e5c78455093ba7ccd
Remco Stoeten <remcostoeten@hotmail.com>
2024-08-06 02:57:40 +0200

    feat: Add develop utillity tailwind showcase helper

---
+++ src/app/(tests)/design-system/components/tailwindColors.ts
@@ -0,0 +1,22 @@
+import config from '../../../../../tailwind.config';
+
+export function GetCustomColors() {
+  const colors = config.theme?.extend?.colors || {};
+  const customColors: Record<string, string> = {};
+
+  Object.entries(colors).forEach(([key, value]) => {
+    if (typeof value === 'string') {
+      customColors[key] = value;
+    } else if (typeof value === 'object') {
+      Object.entries(value).forEach(([subKey, subValue]) => {
+        if (subKey === 'DEFAULT') {
+          customColors[key] = subValue as string;
+        } else {
+          customColors[`${key}-${subKey}`] = subValue as string;
+        }
+      });
+    }
+  });
+
+  return customColors;
+}
--- src/core/types/global.d.ts
+++ src/core/types/global.d.ts
@@ -2,10 +2,10 @@
 import { ReactNode } from 'react';
 
 declare global {
-    type PageProps = {
-        children: ReactNode;
-    };
+  type PageProps = {
+    children: ReactNode;
+  };
 }
 
 // It's important to include this line to make it a module
-export { };
+export {};
