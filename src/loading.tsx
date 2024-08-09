4c52ee09162ba3332a89326824f8d68470ef1af4
Remco Stoeten <remcostoeten@hotmail.com>
2024-08-09 19:31:45 +0200

    refactor: Refactor entire scss setup and build atoms

---
+++ src/loading.tsx
@@ -0,0 +1,10 @@
+import { Center } from './components/atoms/Center';
+import SpinneCodepen from './components/effect/spinner-codepen';
+
+export default function Loading() {
+  return (
+    <Center as="main" fullScreen>
+      <SpinneCodepen />
+    </Center>
+  );
+}
