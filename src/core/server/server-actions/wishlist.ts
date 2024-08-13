4a157cfca18fbd6c3b5894f3ea4fa39d68f8b691
Remco Stoeten <remcostoeten@hotmail.com>
2024-08-14 01:09:05 +0200

    Add invidiual items to db

--- src/core/server/server-actions/wishlist.ts
+++ src/core/server/server-actions/wishlist.ts
@@ -1,8 +1,9 @@
 "use server";
 
+import { eq } from "drizzle-orm";
 import { db } from "@/core/server/db";
 import { v4 as uuidv4 } from "uuid";
-import { wishlists } from "../db/schema";
+import { wishlists, wishlistItems } from "../db/schema";
 
 export async function createWishlist(
   userId: string, 
@@ -13,4 +14,29 @@ export async function createWishlist(
   await db
     .insert(wishlists)
     .values({ id, name, budget, userId });
+}
+
+export async function getWishlistsByUser(userId: string) {
+  return await db
+    .select()
+    .from(wishlists)
+    .where(eq(wishlists.userId, userId));
+}
+
+export async function createWishlistItem(
+  wishlistId: string,
+  name: string,
+  price: number
+) {
+  const id = uuidv4();
+  await db
+    .insert(wishlistItems)
+    .values({ id, name, price, wishlistId });
+}
+
+export async function getWishlistItemsByWishlist(wishlistId: string) {
+  return await db  
+    .select()
+    .from(wishlistItems)
+    .where(eq(wishlistItems.wishlistId, wishlistId));
 }
