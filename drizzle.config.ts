c27e237088ce46272fc6ca4ff1cfb4953e6f7105
Remco Stoeten <remcostoeten@hotmail.com>
2024-08-25 01:40:42 +0200

    re-add env

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,12 +4,12 @@ import type { Config } from 'drizzle-kit'
 export default {
   driver: 'turso',
   out: './src/core/server/db/migrations',
-  schema: './src/core/server/db/schema/index.ts',
+  schema: './src/core/server/db/schema/relation-remodel/index.ts', //temporaryy
+  //   schema: './src/core/server/db/schema/index.ts',
   dialect: 'sqlite',
   dbCredentials: {
-    url: 'libsql://ppppp-remcostoeten.turso.io',
-    authToken:
-      'eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MjM2NzI2MTIsImlkIjoiN2RiZWZlOWUtZjg0OC00ZTNhLWI0ZTgtOWU5ZjJmZDQ5MTM2In0.mf3XvKpOdzVs5qOiguZxMZWkvccZ5J8TfMs1_5CQ2JC5PxJ5xAK5vs6Jp3NEqIM9M-YCQhadORNbxcao2V0eCg',
+    url: process.env.DB_URL,
+    authToken: process.env.AUTH_TOKEN,
   },
   verbose: true,
 } satisfies Config
